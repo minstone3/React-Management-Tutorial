65f758f271ed0700872bf975e40b44d584fac6ae
James Woo <minstone3@gmail.com>
2020-03-10 00:48:01 +0900

    create customers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [
+    {
+      'id': 1,
+      'image': 'https://placeimg.com/64/64/1',
+      'name': '홍길동1',
+      'birthday': '44444',
+      'gender': '여1',
+      'job': '프로그래머'
+    },
+    {
+        'id': 2,
+        'image': 'https://placeimg.com/64/64/2',
+        'name': '홍길동2',
+        'birthday': '123',
+        'gender': '여2',
+        'job': '프로그래머'
+    },
+    {
+        'id': 3,
+        'image': 'https://placeimg.com/64/64/3',
+        'name': '홍길동3',
+        'birthday': '1223',
+        'gender': '여3',
+        'job': '프로그래머3'
+    },
+
+]
 
 function App() {
   return (
-      <div className="gray-background">
-          <img src={logo} lat="logo" />
-          <h2>Let's deveolop management system!</h2>
-    </div>
+      <div>
+          {
+              customers.map(c => {
+                  return (
+                      <Customer
+                          key={c.id}
+                          id={c.id}
+                          image={c.image}
+                          name={c.name}
+                          birthday={c.birthday}
+                          gender={c.gender}
+                          job={c.job}
+                      />
+
+                  );
+              })
+          }
+      </div>
   );
 }
 
